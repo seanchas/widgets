47e6ff00df5f30230304705d76ca9f0bf3b26a0f
seanchas <seanchas@gmail.com>
2011-10-07 18:08:50 +0400

    WIP

--- app/assets/javascripts/mx.widget.js
+++ app/assets/javascripts/mx.widget.js
@@ -1,11 +1,20 @@
 !(function() {
 	
-	var mx = this.mx || (this.mx = {});
+	var mx = this.mx || (this.mx = {}); mx.widget || (mx.widget = {});
+
+	function query(element) {
+		return isNode(element) ? element : document.getElementById(element);
+	}
+
+	function isNode(el) {
+		return (el && el.nodeType && (el.nodeType == 1 || el.nodeType == 9));
+	}
 	
+
 	// entry point
 	
-	mx.table = function(element, engine, market, securities) {
-		
+	mx.widget.table = function(element, options) {
+		query(element).innerHTML = '<iframe src="http://widgets.dev/widget" scrolling="no" frameborder="0" style="border:none; overflow:hidden;" allowTransparency="true"></iframe>';
 	}
 	
 })();
