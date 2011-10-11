4ccb2c81c2ef5c4b3ec28c2700857cdb987da7ac
seanchas <seanchas@gmail.com>
2011-10-11 18:59:02 +0400

    WIP

--- app/assets/javascripts/mx.widget.js
+++ app/assets/javascripts/mx.widget.js
@@ -2,19 +2,32 @@
 	
 	var mx = this.mx || (this.mx = {}); mx.widget || (mx.widget = {});
 
-	function query(element) {
-		return isNode(element) ? element : document.getElementById(element);
-	}
+	function t(s, d) { for (var p in d) s = s.replace(new RegExp('{{' + p + '}}', 'g'), d[p]); return s; }
 
-	function isNode(el) {
-		return (el && el.nodeType && (el.nodeType == 1 || el.nodeType == 9));
+	function query(e) { return isNode(e) ? e : document.getElementById(e); }
+
+	function isNode(e) { return (e && e.nodeType && (e.nodeType == 1 || e.nodeType == 9)); }
+
+	function url(engine, market, params) {
+		return t('http://beta.micex.ru/widgets/widget?engine={{engine}}&market={{market}}&params={{params}}', {
+			engine: encodeURIComponent(engine),
+			market: encodeURIComponent(market),
+			params: encodeURIComponent(params.join(','))
+		});
 	}
 	
+	function i(e) {
+		
+	}
 
 	// entry point
 	
-	mx.widget.table = function(element, options) {
-		query(element).innerHTML = '<iframe src="http://widgets.dev/widget" scrolling="no" frameborder="0" style="border:none; overflow:hidden;" allowTransparency="true"></iframe>';
+	mx.widget.table = function(element, engine, market, params, options) {
+		element = query(element); if (!element) return;
+		
+		query(element).innerHTML = t('<iframe src="{{url}}" style="border: none;"></iframe>', {
+			url: url(engine, market, params)
+		});
 	}
 
 })();
