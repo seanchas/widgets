825a410ad6ab4784adb677b0a69a2fc94d8e7da4
Eugene Kovalev <seanchas@gmail.com>
2011-12-16 17:09:19 +0400

    added version

---
+++ config/initializers/version.rb
@@ -0,0 +1,12 @@
+module Widgets
+    module VERSION
+    
+      MAJOR = 1
+      MINOR = 0
+      TINY  = 0
+      PRE   = 'beta'
+    
+      STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
+    
+    end
+end
